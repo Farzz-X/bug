648125d249f74d28ec56d343b6db1fe1cda0f561
Farzz-X <162881496+Farzz-X@users.noreply.github.com>
2025-11-01 13:54:25 +0700

    Delete database directory

--- database/config.js
+++
@@ -1,7 +0,0 @@
-//credit By Limzzzhama 
-//no hapus credit ketauan hapus hitam//
-
-module.exports = {
-  domain: "http://pablo.hostkita.xyz", // Ubah jadi Domain panel Mu !!!
-  port: "2420" // Ubah Jadi Port Panel Mu !!!
-};
