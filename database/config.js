6b91548599be5fc7f0370ba7588114329d2095c0
Farzz-X <162881496+Farzz-X@users.noreply.github.com>
2025-11-01 13:48:25 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,7 @@
+//credit By Limzzzhama 
+//no hapus credit ketauan hapus hitam//
+
+module.exports = {
+  domain: "http://pablo.hostkita.xyz", // Ubah jadi Domain panel Mu !!!
+  port: "2420" // Ubah Jadi Port Panel Mu !!!
+};
